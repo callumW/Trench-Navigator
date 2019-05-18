75fc061df821e1ec65b3178fbb6c51f34b1989d9
Callum Wilson <calwilson@live.co.uk>
2019-05-18 09:09:26 +0100

    Player moves to touched position

--- Trench Navigator/GameScene.swift
+++ Trench Navigator/GameScene.swift
@@ -12,11 +12,30 @@ import GameplayKit
 class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "Submarine")
     
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.white
         
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         
         addChild(player)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            print("failed to get touch in touchesEnd()")
+            return
+        }
+        
+        let touchLocation = touch.location(in: self)
+        
+        addWaypoint(touchLocation)
+    }
+    
+    /*
+        Add waypoint to player path
+    */
+    func addWaypoint(_ point: CGPoint) {
+        player.run(SKAction.sequence([SKAction.move(to: point, duration: 1.0)]))
     }
 }
