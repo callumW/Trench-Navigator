43e2dc061fd09d6d2bd49c3f11796e0f81c0c967
Callum Wilson <calwilson@live.co.uk>
2019-05-22 13:02:49 +0100

    Interim checkin, expanding line class to have a type (horizontal, vertical, gradient)

--- Trench Navigator/Math.swift
+++ Trench Navigator/Math.swift
@@ -9,16 +9,41 @@
 import Foundation
 import CoreGraphics
 
+enum LineType {
+    case VERTICAL
+    case HORIZONTAL
+    case GRADIENT
+}
+
 /*
     Object defining line in form y = mx + c
 */
 class Line {
     let m: Double
-    let c: Double
+    let c: Double   // y - axis intercept
+    let d: Double   // x - axis intercept
+    let type: LineType
     
     init(a: CGPoint, b: CGPoint) {
-        m = Double(a.y - b.y) / Double(a.x - b.x)
-        c = Double(a.y) - m * Double(a.x)
+        // TODO how to meaningfuly check whether line is horizontal / vertical
+        if a.x == b.x { // horizontal line
+            type = LineType.HORIZONTAL
+            c = Double(a.x)
+            m = 0.0
+            d = Double.infinity
+        }
+        else if a.y == b.y { // vertical line
+            type = LineType.VERTICAL
+            c = Double.infinity
+            m = Double.infinity
+            d = Double(a.y)
+        }
+        else {
+            type = LineType.GRADIENT
+            m = Double(a.y - b.y) / Double(a.x - b.x)
+            c = Double(a.y) - m * Double(a.x)
+            d = (0.0 - c) / m
+        }
     }
     
     /*
@@ -27,19 +52,27 @@ class Line {
     */
     func intersectionPoint(line: Line) -> CGPoint? {
         
-        let deltaC = line.c - self.c
-        
-        let deltaM = self.m - line.m
-        
-        if deltaM == 0.0 {
+        switch (self.type) {
+        case .GRADIENT:
+            let deltaC = line.c - self.c
+            let deltaM = self.m - line.m
             
+            let x = deltaC / deltaM
+            let y = self.getY(x) 
+            break;
+        case .HORIZONTAL:
+            
+            break;
+        case .VERTICAL:
+            break;
         }
         
-
-        let x = (line.c - self.c) / (self.m - line.m)
-        
         // now we can workout y
-        let y = self.getY(x)
+        
+        
+        if y.isInfinite {
+            return nil
+        }
         
         return CGPoint(x: x, y: y)
     }
